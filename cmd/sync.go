e382c359138cc9b4f066e3dc581572fa7a8754ca
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-07-08 18:22:34 -0400

    added SyncDir function, SyncDir function uploaded a dir(with all its files) recurrently to google drive ok

--- cmd/sync.go
+++ cmd/sync.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
@@ -77,6 +78,54 @@ func saveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
+//Sync a folder recurrently to google drive.
+func SyncDir(dir string, parent []string, srv *drive.Service) {
+
+	//read current dir
+	currentDirFiles, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Unable to read dir: %v", err)
+	}
+
+	driveFolderName := filepath.Base(dir)
+
+	folderMeta := &drive.File{
+		Name:     driveFolderName,
+		MimeType: "application/vnd.google-apps.folder",
+		Parents:  parent,
+	}
+	driveFolder, err := srv.Files.Create(folderMeta).Do()
+	if err != nil {
+		log.Fatalf("Unable to create Drive folder: %v", err)
+	}
+
+	for _, file := range currentDirFiles {
+		if file.IsDir() {
+			SyncDir(path.Join(dir, file.Name()), []string{driveFolder.Id}, srv)
+		} else {
+			fileMeta := &drive.File{
+				Name:    file.Name(),
+				Parents: []string{driveFolder.Id},
+			}
+
+			f, err := os.Open(path.Join(dir, file.Name()))
+			if err != nil {
+				log.Fatalf("Unable to open file %q %v", file.Name(), err)
+			}
+
+			defer f.Close()
+
+			driveFile, err := srv.Files.Create(fileMeta).Media(f).Do()
+			if err != nil {
+				log.Fatalf("Unable to create %q in google drive %v", file.Name(), err)
+			}
+
+			fmt.Printf("Uploaded file %q Id %v to google drive\n", file.Name(), driveFile.Id)
+
+		}
+	}
+}
+
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
@@ -84,6 +133,7 @@ var syncCmd = &cobra.Command{
 	Long: `Sync/backup the current directory if no argument is passed
 or sync/backup the file or directory specified in the args:
  - dsync sync [file/directory]`,
+	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		b, err := ioutil.ReadFile(filepath.Join(userHome, ".dsync/client_secret_654016737032-1jj92r0pcflivhq85nh31fim8fhlr1o7.apps.googleusercontent.com.json"))
 		if err != nil {
@@ -102,21 +152,18 @@ or sync/backup the file or directory specified in the args:
 			log.Fatalf("Unable to retrieve Drive client: %v", err)
 		}
 
-		currentDir, err := os.Getwd()
-		if err != nil {
-			log.Fatalf("Unable to get working directory: %v", err)
+		var dirToSync string
+		if len(args) == 0 {
+			currentDir, err := os.Getwd()
+			if err != nil {
+				log.Fatalf("Unable to get working directory: %v", err)
+			}
+			dirToSync = currentDir
+		} else {
+			dirToSync = args[0]
 		}
-		driveFolderName := filepath.Base(currentDir)
 
-		fileMeta := &drive.File{
-			Name:     driveFolderName,
-			MimeType: "application/vnd.google-apps.folder",
-		}
-		driveFolder, err := srv.Files.Create(fileMeta).Do()
-		if err != nil {
-			log.Fatalf("Unable to create Drive folder: %v", err)
-		}
-		fmt.Println(driveFolder.Id)
+		SyncDir(dirToSync, nil, srv)
 
 	},
 }
