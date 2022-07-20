2ac515f9e32b52ea440618e606659f8a1f2118aa
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-07-19 21:20:24 -0400

    'list' command working ok

--- cmd/list.go
+++ cmd/list.go
@@ -6,6 +6,9 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path"
 
 	"github.com/spf13/cobra"
 )
@@ -13,15 +16,15 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List all sync tasks",
+	Long: `List all sync tasks:
+"dsync list".`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		listFileData, err := os.ReadFile(path.Join(UserHome, ".dsync/tasks.dsync"))
+		if err != nil {
+			log.Fatalf("Unable to read tasks list file: %v", err)
+		}
+		fmt.Println(string(listFileData))
 	},
 }
 
